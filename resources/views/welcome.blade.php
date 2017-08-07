534acee6ac6e7c3e59ef492ecc42352fcf150974
Mike Bywater <mike.j.bywater@gmail.com>
2017-08-07 02:46:00 +0100

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/app.css">
+        <script src="js/app.js"></script>
+
+        <title>Promotion Wars</title>
+
+    </head>
+    <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="img/banner.jpg" height="40"/></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mike Bywater <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Feedback</a></li>
+                            <li><a href="#">Inbox</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">WWE <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Championships</a></li>
+                            <li><a href="#">Feuds</a></li>
+                            <li><a href="#">Tag Teams / Stables</a></li>
+                            <li><a href="#">Training Camp</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Roster</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Shows</a></li>
+                        </ul>
+                    </li>
+
+
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Game World <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Promotions</a></li>
+                            <li><a href="#">Wrestlers</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Next Day</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </body>
+</html>
