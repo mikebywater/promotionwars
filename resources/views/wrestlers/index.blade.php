883784c75e37d48de547025c11690bc326a34215
Mike Bywater <mike.bywater@enta.net>
2017-08-10 03:09:15 +0100

    Updated the migration

--- resources/views/wrestlers/index.blade.php
+++ resources/views/wrestlers/index.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>Wrestlers</h3>
-    <table class = "table table-striped">
-        <tr>
-            <th>Name</th>
-            <th>Draw</th>
-            <th>Ability</th>
-        </tr>
-        @foreach($wrestlers as $wrestler)
-            <tr>
-                <td>{{$wrestler->name}}</td>
-                <td>{{$wrestler->draw}}</td>
-                <td>{{$wrestler->ability}}</td>
-            </tr>
-        @endforeach
+ <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <table class = "table table-striped panel-primary">
+                    <tr>
+                        <th>Name</th>
+                        <th>Draw</th>
+                        <th>Ability</th>
+                        <th>Promotion</th>
+                    </tr>
+                    @foreach($wrestlers as $wrestler)
+                        <tr>
+                            <td><a href="/wrestlers/{{$wrestler->id}}">{{$wrestler->name}}</a></td>
+                            <td>{{$wrestler->draw}}</td>
+                            <td>{{$wrestler->ability}}</td>
+                            <td>{{$wrestler->promotion_id}}</td>
+                        </tr>
+                    @endforeach
 
-    </table>
+                </table>
+            </div>
+        </div>
+    </div>
+ </div>
 
 
 @endsection
